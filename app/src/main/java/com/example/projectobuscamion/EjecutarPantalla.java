35bda3c7b0c5e59b8f2d6868edc484645d04350b
Yhunn <thedarkking29@gmail.com>
2021-05-03 14:27:34 -0600

    correcciones de las pantallas

---
+++ app/src/main/java/com/example/projectobuscamion/EjecutarPantalla.java
@@ -0,0 +1,14 @@
+package com.example.projectobuscamion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EjecutarPantalla extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.propuesta_mapa);
+    }
+}
